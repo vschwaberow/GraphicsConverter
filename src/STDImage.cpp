6c7f611c895314555d8d7a65bfb95f750e55fca3
Volker Schwaberow <volker@schwaberow.de>
2024-07-06 09:46:58 +0200

    feat: Add STBImage implementation for image loading

---
+++ src/STDImage.cpp
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+// Project: gfxconverter
+// File: src/STDImage.cpp
+// Author: Volker Schwaberow <volker@schwaberow.de>
+// Copyright (c) 2022 Volker Schwaberow
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
