d10a56b023a8b2397958b3dccd4f54efea2116cd
Volker Schwaberow <volker@schwaberow.de>
2024-07-07 19:34:54 +0200

    chore: Update ColorReducer.h with OctreeNode and ColorBox classes for color reduction algorithm

--- src/ColorReducer.cpp
+++ src/ColorReducer.cpp
@@ -6,6 +6,14 @@
 
 #include "ColorReducer.h"
 
+int colorDistance(const Color &c1, const Color &c2)
+{
+    int dr = c1.r - c2.r;
+    int dg = c1.g - c2.g;
+    int db = c1.b - c2.b;
+    return dr * dr + dg * dg + db * db;
+}
+
 std::vector<uint32_t> ColorReducer::reduceColors(const std::vector<uint32_t> &image, int width, int height, int targetColors, ColorReductionAlgorithm algo)
 {
     switch (algo)
@@ -23,18 +31,224 @@ std::vector<uint32_t> ColorReducer::reduceColors(const std::vector<uint32_t> &im
 
 std::vector<uint32_t> ColorReducer::medianCut(const std::vector<uint32_t> &image, int targetColors)
 {
-    // TODO: Implement median cut algorithm
-    return image;
+    auto compareBoxes = [](const ColorBox &a, const ColorBox &b)
+    {
+        return a.pixels.size() < b.pixels.size();
+    };
+    std::priority_queue<ColorBox, std::vector<ColorBox>, decltype(compareBoxes)> boxes(compareBoxes);
+
+    boxes.push(ColorBox(image));
+
+    while (static_cast<int>(boxes.size()) < targetColors)
+    {
+        ColorBox box = boxes.top();
+        boxes.pop();
+
+        if (box.pixels.size() < 2)
+        {
+            boxes.push(box);
+            break;
+        }
+
+        int splitIndex = box.getLongestSideIndex();
+        std::sort(box.pixels.begin(), box.pixels.end(), [splitIndex](uint32_t a, uint32_t b)
+                  { return ((a >> (8 * (2 - splitIndex))) & 0xFF) < ((b >> (8 * (2 - splitIndex))) & 0xFF); });
+
+        size_t medianIndex = box.pixels.size() / 2;
+        boxes.push(ColorBox(std::vector<uint32_t>(box.pixels.begin(), box.pixels.begin() + medianIndex)));
+        boxes.push(ColorBox(std::vector<uint32_t>(box.pixels.begin() + medianIndex, box.pixels.end())));
+    }
+
+    std::vector<uint32_t> palette;
+    while (!boxes.empty())
+    {
+        const ColorBox &box = boxes.top();
+        int r = 0, g = 0, b = 0;
+        for (uint32_t pixel : box.pixels)
+        {
+            r += (pixel >> 16) & 0xFF;
+            g += (pixel >> 8) & 0xFF;
+            b += pixel & 0xFF;
+        }
+        r /= box.pixels.size();
+        g /= box.pixels.size();
+        b /= box.pixels.size();
+        palette.push_back((r << 16) | (g << 8) | b);
+        boxes.pop();
+    }
+
+    std::vector<uint32_t> result(image.size());
+    for (size_t i = 0; i < image.size(); ++i)
+    {
+        uint32_t pixel = image[i];
+        int r = (pixel >> 16) & 0xFF;
+        int g = (pixel >> 8) & 0xFF;
+        int b = pixel & 0xFF;
+
+        uint32_t closestColor = palette[0];
+        int minDistance = std::numeric_limits<int>::max();
+
+        for (uint32_t paletteColor : palette)
+        {
+            int pr = (paletteColor >> 16) & 0xFF;
+            int pg = (paletteColor >> 8) & 0xFF;
+            int pb = paletteColor & 0xFF;
+
+            int distance = (r - pr) * (r - pr) + (g - pg) * (g - pg) + (b - pb) * (b - pb);
+            if (distance < minDistance)
+            {
+                minDistance = distance;
+                closestColor = paletteColor;
+            }
+        }
+
+        result[i] = closestColor;
+    }
+
+    return result;
 }
 
 std::vector<uint32_t> ColorReducer::kMeans(const std::vector<uint32_t> &image, int targetColors)
 {
-    // TODO: Implement k-means algorithm
-    return image;
+    std::vector<Color> pixels;
+    pixels.reserve(image.size());
+    for (uint32_t pixel : image)
+    {
+        pixels.emplace_back(pixel);
+    }
+
+    std::vector<Color> centroids;
+    centroids.reserve(targetColors);
+
+    // Initialize centroids randomly
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(0, image.size() - 1);
+    for (int i = 0; i < targetColors; ++i)
+    {
+        centroids.push_back(pixels[dis(gen)]);
+    }
+
+    std::vector<int> assignments(pixels.size());
+    bool changed = true;
+    int maxIterations = 100;
+
+    while (changed && maxIterations > 0)
+    {
+        changed = false;
+
+        for (size_t i = 0; i < pixels.size(); ++i)
+        {
+            int nearestCentroid = 0;
+            int minDistance = std::numeric_limits<int>::max();
+
+            for (int j = 0; j < targetColors; ++j)
+            {
+                int distance = colorDistance(pixels[i], centroids[j]);
+                if (distance < minDistance)
+                {
+                    minDistance = distance;
+                    nearestCentroid = j;
+                }
+            }
+
+            if (assignments[i] != nearestCentroid)
+            {
+                assignments[i] = nearestCentroid;
+                changed = true;
+            }
+        }
+
+        std::vector<int> counts(targetColors, 0);
+        std::vector<Color> newCentroids(targetColors);
+
+        for (size_t i = 0; i < pixels.size(); ++i)
+        {
+            int centroidIndex = assignments[i];
+            newCentroids[centroidIndex].r += pixels[i].r;
+            newCentroids[centroidIndex].g += pixels[i].g;
+            newCentroids[centroidIndex].b += pixels[i].b;
+            counts[centroidIndex]++;
+        }
+
+        for (int i = 0; i < targetColors; ++i)
+        {
+            if (counts[i] > 0)
+            {
+                newCentroids[i].r /= counts[i];
+                newCentroids[i].g /= counts[i];
+                newCentroids[i].b /= counts[i];
+            }
+        }
+
+        centroids = newCentroids;
+        maxIterations--;
+    }
+
+    std::vector<uint32_t> result(image.size());
+    for (size_t i = 0; i < pixels.size(); ++i)
+    {
+        result[i] = centroids[assignments[i]].toUint32();
+    }
+
+    return result;
 }
 
 std::vector<uint32_t> ColorReducer::octreeQuantization(const std::vector<uint32_t> &image, int targetColors)
 {
-    // TODO: Implement octree quantization algorithm
-    return image;
+    OctreeNode root;
+    int leafCount = 0;
+    int maxLevel = 8;
+
+    for (uint32_t color : image)
+    {
+        root.addColor(color, 0, maxLevel, leafCount, targetColors);
+    }
+
+    std::vector<uint32_t> palette;
+    std::queue<OctreeNode *> queue;
+    queue.push(&root);
+
+    while (!queue.empty() && palette.size() < static_cast<size_t>(targetColors))
+    {
+        OctreeNode *node = queue.front();
+        queue.pop();
+
+        if (node->isLeaf)
+        {
+            palette.push_back(node->getColor());
+        }
+        else
+        {
+            for (int i = 0; i < 8; ++i)
+            {
+                if (node->children[i] != nullptr)
+                {
+                    queue.push(node->children[i]);
+                }
+            }
+        }
+    }
+
+    std::vector<uint32_t> result(image.size());
+    for (size_t i = 0; i < image.size(); ++i)
+    {
+        uint32_t originalColor = image[i];
+        uint32_t nearestColor = palette[0];
+        int minDistance = std::numeric_limits<int>::max();
+
+        for (uint32_t paletteColor : palette)
+        {
+            int distance = colorDistance(Color(originalColor), Color(paletteColor));
+            if (distance < minDistance)
+            {
+                minDistance = distance;
+                nearestColor = paletteColor;
+            }
+        }
+
+        result[i] = nearestColor;
+    }
+
+    return result;
 }
--- src/ColorReducer.h
+++ src/ColorReducer.h
@@ -4,16 +4,141 @@
 // Author: Volker Schwaberow <volker@schwaberow.de>
 // Copyright (c) 2022 Volker Schwaberow
 
-#ifndef COLOR_REDUCER_H
-#define COLOR_REDUCER_H
+#pragma once
+
 #include <vector>
 #include <cstdint>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <limits>
+#include <random>
 
 enum class ColorReductionAlgorithm
 {
     MedianCut,
     KMeans,
     OctreeQuantization
+};
+
+struct ColorBox
+{
+    std::vector<uint32_t> pixels;
+    int minR, maxR, minG, maxG, minB, maxB;
+
+    ColorBox(const std::vector<uint32_t> &pixels) : pixels(pixels)
+    {
+        minR = minG = minB = 255;
+        maxR = maxG = maxB = 0;
+        for (uint32_t pixel : pixels)
+        {
+            int r = (pixel >> 16) & 0xFF;
+            int g = (pixel >> 8) & 0xFF;
+            int b = pixel & 0xFF;
+            minR = std::min(minR, r);
+            maxR = std::max(maxR, r);
+            minG = std::min(minG, g);
+            maxG = std::max(maxG, g);
+            minB = std::min(minB, b);
+            maxB = std::max(maxB, b);
+        }
+    }
+
+    int getLongestSideIndex() const
+    {
+        int rLength = maxR - minR;
+        int gLength = maxG - minG;
+        int bLength = maxB - minB;
+        if (rLength >= gLength && rLength >= bLength)
+            return 0;
+        if (gLength >= rLength && gLength >= bLength)
+            return 1;
+        return 2;
+    }
+};
+
+struct Color
+{
+    int r, g, b;
+    Color() : r(0), g(0), b(0) {}
+    Color(uint32_t pixel) : r((pixel >> 16) & 0xFF), g((pixel >> 8) & 0xFF), b(pixel & 0xFF) {}
+    uint32_t toUint32() const { return (r << 16) | (g << 8) | b; }
+};
+
+class OctreeNode
+{
+public:
+    OctreeNode() : red(0), green(0), blue(0), pixelCount(0), isLeaf(false)
+    {
+        for (int i = 0; i < 8; ++i)
+        {
+            children[i] = nullptr;
+        }
+    }
+
+    ~OctreeNode()
+    {
+        for (int i = 0; i < 8; ++i)
+        {
+            delete children[i];
+        }
+    }
+
+    void addColor(uint32_t color, int level, int maxLevel, int &leafCount, int maxLeaves)
+    {
+        ++pixelCount;
+        red += (color >> 16) & 0xFF;
+        green += (color >> 8) & 0xFF;
+        blue += color & 0xFF;
+
+        if (level >= maxLevel)
+        {
+            isLeaf = true;
+            return;
+        }
+
+        int index = getColorIndex(color, level);
+        if (children[index] == nullptr)
+        {
+            if (leafCount >= maxLeaves)
+                return;
+            children[index] = new OctreeNode();
+            ++leafCount;
+        }
+        children[index]->addColor(color, level + 1, maxLevel, leafCount, maxLeaves);
+    }
+
+    uint32_t getColor() const
+    {
+        if (pixelCount == 0)
+            return 0;
+        int r = red / pixelCount;
+        int g = green / pixelCount;
+        int b = blue / pixelCount;
+        return (r << 16) | (g << 8) | b;
+    }
+
+    OctreeNode *children[8];
+    int red, green, blue;
+    int pixelCount;
+    bool isLeaf;
+
+private:
+    int getColorIndex(uint32_t color, int level)
+    {
+        int r = (color >> 16) & 0xFF;
+        int g = (color >> 8) & 0xFF;
+        int b = color & 0xFF;
+        int index = 0;
+        int mask = 0x80 >> level;
+        if (r & mask)
+            index |= 4;
+        if (g & mask)
+            index |= 2;
+        if (b & mask)
+            index |= 1;
+        return index;
+    }
 };
 
 class ColorReducer
@@ -26,5 +151,3 @@ private:
     static std::vector<uint32_t> kMeans(const std::vector<uint32_t> &image, int targetColors);
     static std::vector<uint32_t> octreeQuantization(const std::vector<uint32_t> &image, int targetColors);
 };
-
-#endif // COLOR_REDUCER_H
